f3775f38b2e8257c54405dac973383a1781d3154
BoraDere <boradere@hotmail.com.tr>
2024-07-13 09:33:58 +0300

    fix memory bloat

--- asd.py
+++ asd.py
@@ -9,6 +9,7 @@ import sys
 from concurrent.futures import ProcessPoolExecutor
 import os
 import pickle
+import gc
 
 THRESHOLD = 0.6
 AUTHORIZED_USERS_DIR = 'authorized_users'
@@ -39,9 +40,9 @@ authorized_encodings = {}
 """
 test cases:
 halt while:
-    DONE capturing tick
-    DONE initializing auth encodings tick
-    after the capturing tickkk
+    DONE capturing
+    DONE initializing auth encodings
+    after the capturing, "30 secs of wait time waits for the whole 30 sec"
     DONE nth capturing
     DONE unauth detecting
 """
@@ -80,7 +81,7 @@ def show_error_message(message: str) -> None:
     root.destroy()
 
 
-def encode_face(face_image) -> list|None:
+def encode_face(face_image) -> None:
     """
     Function to encode faces using face_recognition.
 
@@ -88,7 +89,7 @@ def encode_face(face_image) -> list|None:
         face_image(cv2 frame): Cropped face image.
 
     Returns:
-        encodings: Face encoding if face_encodings didn't return None, else None.
+        encodings: Face encodings if face_encodings didn't return None, else None.
     """
     # using hog model to increase the performance a bit
     encodings = face_recognition.face_encodings(face_image, model='hog')
@@ -131,52 +132,52 @@ def encode_face(face_image) -> list|None:
 
 
 def load_or_generate_encodings():
-    # print("function starts")
+    print("function starts")
     authorized_encodings = {}
     if os.path.exists(AUTHORIZED_ENCODINGS_DIR):
-        # print("in if loop")
+        print("in if loop")
         # Load existing encodings
         for user_dir in os.listdir(AUTHORIZED_ENCODINGS_DIR):
-            # print(f'user_dir: {user_dir}')
+            print(f'user_dir: {user_dir}')
             user_encodings = []
             user_path = os.path.join(AUTHORIZED_ENCODINGS_DIR, user_dir)
-            # print(f'user_path: {user_path}')
+            print(f'user_path: {user_path}')
             for encoding_file in os.listdir(user_path):
                 with open(os.path.join(user_path, encoding_file), 'rb') as f:
                     encoding = pickle.load(f)
-                    # print(f'encoding: {encoding}')
+                    print(f'encoding: {encoding}')
                     user_encodings.append(encoding)
             authorized_encodings[user_dir] = user_encodings
-        # print('if bitişi')
+        print('if bitişi')
     else:
         # Generate encodings
-        # print('in else loop')
+        print('in else loop')
         os.makedirs(AUTHORIZED_ENCODINGS_DIR, exist_ok=True)
         for user_dir in os.listdir(AUTHORIZED_USERS_DIR):
-            # print(f'user_dir: {user_dir}')
+            print(f'user_dir: {user_dir}')
             user_path = os.path.join(AUTHORIZED_USERS_DIR, user_dir)
-            # print(f'user_path: {user_path}')
+            print(f'user_path: {user_path}')
             user_encodings_dir = os.path.join(AUTHORIZED_ENCODINGS_DIR, user_dir)
-            # print(f'user_encodings_dir: {user_encodings_dir}')
+            print(f'user_encodings_dir: {user_encodings_dir}')
             os.makedirs(user_encodings_dir, exist_ok=True)
             user_encodings = []
             for image_file in os.listdir(user_path):
                 image_path = os.path.join(user_path, image_file)
-                # print(f'image_path: {image_path}')
+                print(f'image_path: {image_path}')
                 image = face_recognition.load_image_file(image_path)
                 encodings = face_recognition.face_encodings(image)
                 if encodings:
                     encoding = encodings[0]
-                    # print(f'encoding: {encoding}')
+                    print(f'encoding: {encoding}')
                     user_encodings.append(encoding)
                     # Save encoding
                     encoding_file_path = os.path.join(user_encodings_dir, image_file + "_Encoding")
                     with open(encoding_file_path, 'wb') as f:
                         pickle.dump(encoding, f)
-                # else:
-                #     print(f"No face encodings found in {image_file}. Skipping.")
+                else:
+                    print(f"No face encodings found in {image_file}. Skipping.")
             authorized_encodings[user_dir] = user_encodings
-        # print('else bitişi')
+        print('else bitişi')
     return authorized_encodings
 
 
@@ -195,7 +196,7 @@ def settings_reader(filename: str):
         sys.exit()
 
 
-def capture(camera: str, show_frame: str, capture_duration: int, block_multi_user: bool, cap: cv2.VideoCapture) -> None:
+def capture(camera: str, show_frame: str, capture_duration: int, block_multi_user: bool) -> None:
     """
     Main function that is responsible of capturing, detecting and recognizing.
 
@@ -206,6 +207,7 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
     """
     global running, authorized_encodings, process_current_frame
 
+    cap = cv2.VideoCapture(camera)
     authorized_detected = False
     
     # error while opening the camera
@@ -217,9 +219,7 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
     start_time = time.time()
 
     while time.time() - start_time < capture_duration and running and not authorized_detected:
-    # while running and not authorized_detected:
         ret, frame = cap.read()
-
         if not ret:
             # if not ret cap.read() doesn't return a frame, meaning there is a problem. which is mostly the camera being already used
             message = f"{camera} numaralı kamera şu anda kullanımda veya başka bir hata oluştu."
@@ -228,8 +228,9 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
 
         if process_current_frame:
             # performance-wise operation
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)            
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
             rgb_small_frame = small_frame[:, :, ::-1]  # BGR to RGB conversion
+            del small_frame
             face_locations = face_recognition.face_locations(rgb_small_frame, model='hog')
             # debugging
             # print("Face locations:", face_locations)
@@ -249,6 +250,7 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
             if faces:
                 with ProcessPoolExecutor() as executor:
                     face_encodings = list(executor.map(encode_face, faces))
+                del faces
             else:
                 # so that compare_faces won't receive None
                 # kinda unnecessary thanks to valid_face_encodings, check if this is the case
@@ -256,6 +258,7 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
 
             unauthorized_detected = False
             valid_face_encodings = [enc for enc in face_encodings if enc is not None]
+            del face_encodings
 
             for encoding in valid_face_encodings:
                 matches = face_recognition.compare_faces(
@@ -265,20 +268,20 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
                 )
 
                 if block_multi_user:
-                    # print('yes block_multi_user')
+                    print('yes block_multi_user')
                     # order of these two ifs can be used in block_multi_user??
                     if not any(matches):
                         unauthorized_detected = True
                         break
 
                     if any(matches):  # This condition is met if an authorized person is detected
-                        # print("Authorized person detected. Stopping capture.")
+                        print("Authorized person detected. Stopping capture.")
                         authorized_detected = True
                         break  # Exit the loop to stop capturing
                 else:
                     print('no block_multi_user')
                     if any(matches):  # This condition is met if an authorized person is detected
-                        # print("Authorized person detected. Stopping capture.")
+                        print("Authorized person detected. Stopping capture.")
                         authorized_detected = True
                         break  # Exit the loop to stop capturing
 
@@ -286,6 +289,7 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
                         unauthorized_detected = True
                         break
 
+            del valid_face_encodings
 
             if unauthorized_detected:
                 # change it from being an immediate action with window panes
@@ -297,31 +301,39 @@ def capture(camera: str, show_frame: str, capture_duration: int, block_multi_use
                 break
 
             if show_frame:
-                # time.sleep(5)
                 for top, right, bottom, left in face_locations:
                     top *= 4; right *= 4; bottom *= 4; left *= 4 
                     cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-            cv2.imshow('Webcam', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'): # for debugging purposes
-                break
+                cv2.imshow('Webcam', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'): # for debugging purposes
+                    break
 
         process_current_frame = not process_current_frame # unbound bunun yüzünden
+    del face_locations
+    del rgb_small_frame
+    gc.collect()
 
-    # cap.release()
+    cap.release()
     if show_frame:
         cv2.destroyAllWindows()
 
 
-
-def capture_loop(camera: str, show_frame: str, wait_time, capture_duration: int, block_multi_user: bool):
+def capture_callback():
     """
     Callback function for threading timer.
     """
     global running
     if not running:  
         return
-    
-    cap = cv2.VideoCapture(camera)    
+
+    settings = settings_reader('settings.json')
+    camera = settings['camera']
+    show_frame = str_to_bool(settings['show_frame'])
+    wait_time = settings['wait_time']
+    capture_duration = settings['capture_duration']
+    block_multi_user = str_to_bool(settings['block_multi_user'])
+    capture(camera, show_frame, capture_duration, block_multi_user)
+    if running:  
         # sleep_time = 30  
         # elapsed = 0
         # while running and elapsed < sleep_time:  
@@ -329,29 +341,18 @@ def capture_loop(camera: str, show_frame: str, wait_time, capture_duration: int,
         #     elapsed += 1
         # if running:  
         #     threading.Timer(30, capture_callback).start()
-        # threading.Timer(wait_time, capture_callback).start(),
-    while True:
-        capture(camera, show_frame, capture_duration, block_multi_user, cap)
-        time.sleep(wait_time)  
+        threading.Timer(wait_time, capture_callback).start()
 
 
-def main():
-    global authorized_encodings, running
-    settings = settings_reader('settings.json')
-    camera = settings['camera']
-    show_frame = str_to_bool(settings['show_frame'])
-    wait_time = settings['wait_time']
-    capture_duration = settings['capture_duration']
-    block_multi_user = str_to_bool(settings['block_multi_user'])
+if __name__ == '__main__':
     authorized_encodings = load_or_generate_encodings()
-
+    for user, enc in authorized_encodings.items():
+        print(f'{user}: {enc}')
+    capture_callback()
     try:
-        capture_loop(camera, show_frame, wait_time, capture_duration, block_multi_user)
+        while running:
+            time.sleep(1)
     except KeyboardInterrupt:
         running = False
         print("Program terminated.")
         sys.exit()
-
-
-if __name__ == '__main__':
-    main()
